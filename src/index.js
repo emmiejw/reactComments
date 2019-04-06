826d5d276124ad08dcb8d440429efdae0dd5212c
emmiejw <emilyprice84@outlook.com>
2019-04-06 16:29:05 +0100

    first react.js components for blog comments

--- src/index.js
+++ src/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import  ApprovalCard from './ApprovalCard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = ()  => {
+    return (
+    <div className="ui container comments">
+        <ApprovalCard >
+            <CommentDetail  
+                author="Toby" 
+                date="07/02/2019 1:00AM" 
+                text="GRRRR I'm going to wake Mum up because I need the loo!!!" 
+                avatar={faker.image.avatar()} 
+            />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail 
+                author="Emmie" d
+                ate="07/06/2018 11:15AM" 
+                text="My fist blog post with React.js"
+                avatar={faker.image.avatar()} 
+            />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail 
+                author="Lewis" 
+                date="19/11/2018 03:39PM"
+                text="This was the time I was born on this date!!"
+                avatar={faker.image.avatar()} 
+             />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail 
+                author="Nana" 
+                date="29/06/2018 10:30AM"
+                text="One more year until I'm 60!!"
+                avatar={faker.image.avatar()} 
+             />
+        </ApprovalCard> 
+        <ApprovalCard>
+            <CommentDetail 
+                author="Sebby" 
+                date="01/04/2019 09:30AM"
+                text="Today I'm 30!!"
+                avatar={faker.image.avatar()} 
+             />
+        </ApprovalCard>     
+        </div>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
